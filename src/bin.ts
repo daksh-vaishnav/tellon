d222568c906066b80674433833879145d187cc1b
Dhaval <dhaval@ainocular.com>
2025-12-22 20:23:22 +0530

    update server initialization and add graceful shutdown handling

--- src/bin.ts
+++ src/bin.ts
@@ -1,8 +1,33 @@
+import { config } from "./config/config.js";
 import app from "./index.js"
 
 
-const PORT = 3000
+const port = config.port;
 
-app.listen(PORT, () => {
-    console.log(`app is running on http://localhost:${PORT}`);
+const server = app.listen(port, () => {
+    console.log(`app is running on http://localhost:${port}`);
+})
+
+
+const gracefulShutDown = () => {
+    server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+    })
+}
+
+process.on('SIGINT', gracefulShutDown)
+process.on('SIGTERM', gracefulShutDown)
+
+
+process.on('uncaughtException', (err) => {
+    console.log('uncaughtException');
+    console.log(err);
+    gracefulShutDown();
+})
+
+process.on('unhandledRejection', (reason) => {
+    console.log('unhandledRejection');
+    console.log(reason);
+    gracefulShutDown();
 })
